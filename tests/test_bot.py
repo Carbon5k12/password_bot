0cf36e3edba3de49528d9ffa325996dd98249187
Anton Panfilov <chehonte2001@yandex.ru>
2025-04-29 07:46:12 +0300

    fixed test_bot.py

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -5,10 +5,10 @@ def test_password_generation():
     generator = XKCDPasswordGenerator()
     
     weak = generator.generate("weak")
-    assert len(weak.split()) >= 2  # Проверка количества слов
+    assert len(weak) >= 6 
     
     normal = generator.generate("normal")
-    assert any(char.isdigit() for char in normal)  # Должен содержать цифры
+    assert any(char.isdigit() for char in normal)
     
     strong = generator.generate("strong")
-    assert any(not (char.isalpha() or char.isdigit()) for char in strong)  # Должен содержать спецсимволы
+    assert any(not (char.isalpha() or char.isdigit()) for char in strong)
